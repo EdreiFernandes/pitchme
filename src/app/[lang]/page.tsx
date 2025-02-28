01d11eff8e2a9529774323213d316f2a3d7086a4
Edrei Fernandes <edreifernandes@gmail.com>
2025-02-27 22:34:00 -0300

    Creating locale switcher

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,5 +1,6 @@
 import { getDictionary } from "@/get-dictionary";
 import { Locale } from "../../i18n-config";
+import LocaleSwitcher from "./components/locale-switcher";
 
 export default async function Home(props: {params: Promise<{lang: Locale}>;}) {
   const {lang} = await props.params;
@@ -11,6 +12,8 @@ export default async function Home(props: {params: Promise<{lang: Locale}>;}) {
         <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 dark:bg-gray-800 dark:border-gray-700">
           <h5 className="mb-3 text-base font-semibold text-gray-900 md:text-xl dark:text-white">
             Pitch.Me
+
+            <LocaleSwitcher></LocaleSwitcher>
           </h5>
           <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
             #dev por profissão e nas horas vagas.
